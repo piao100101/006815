bd4b9cbdc35aa30735a540fb89ccf6ffaee07dc6
piaoge <piao100101@gmail.com>
2017-01-10 16:45:27 +0900

    Update data.sql

--- ch02/2.3.4.2_hajiboot-jpa/src/main/resources/data.sql
+++ ch02/2.3.4.2_hajiboot-jpa/src/main/resources/data.sql
@@ -2,3 +2,4 @@ INSERT INTO customers(first_name, last_name) VALUES('Nobita', 'Nobi');
 INSERT INTO customers(first_name, last_name) VALUES('Takeshi', 'Goda');
 INSERT INTO customers(first_name, last_name) VALUES('Suneo', 'Honekawa');
 INSERT INTO customers(first_name, last_name) VALUES('Shizuka', 'Minamoto');
+DSF
